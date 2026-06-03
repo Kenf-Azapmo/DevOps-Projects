3be6f083815422f7a2247543460c7de6ed079bfd
Kenf-Azapmo <azapmokenfack@gmail.com>
2026-06-03 18:45:45 +0100

    uplaoding the viariable for the main VPC manifest

---
+++ DevOps-Project-01/infrastructure/modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+variable "environment" {
+  description = "Environment Name" #The environment for the VPC (e.g., dev, staging, prod)
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones for the subnets"
+  type        = list(string)
+}
