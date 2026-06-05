a77d661e821acb460c9bcb8c7f7e09347fce62e1
Kenf-Azapmo <azapmokenfack@gmail.com>
2026-06-05 07:29:56 +0100

    uploading the main manifest file for the security module

---
+++ DevOps-Project-01/infrastructure/modules/security/main.tf
@@ -0,0 +1,124 @@
+#Security Groupes Module
+
+#ALB Security Gorup
+resource "aws_security_group" "alb" {
+  name        = "${var.environment}-alb-sg"
+  description = "Security group for application load balancer"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0./0"]
+  }
+
+  tags = {
+    Name        = "${var.environment}-alb-sg"
+    Environment = var.environment
+  }
+
+}
+
+
+#Application Security Group
+resource "aws_security_group" "name" {
+  name        = "${var.environment}-app-sg"
+  description = "Security group for application servers"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol = [
+      aws_security_group.alb.id
+    ]
+  }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.baston.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.environment}-app-sg"
+    Environment = var.environment
+  }
+
+}
+
+
+# Database Security Group
+resource "aws_security_group" "db" {
+  name        = "${var.environment}-db-sg"
+  description = "Security group for database servers"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.name.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.environment}-db-sg"
+    Environment = var.environment
+  }
+}
+
+# Bastion Host Security Group
+resource "aws_security_group" "baston" {
+  name        = "${var.environment}-baston-sg"
+  description = "Security group for bastion host"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.environment}-baston-sg"
+    Environment = var.environment
+  }
+}
