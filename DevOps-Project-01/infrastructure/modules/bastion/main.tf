a249327af275ffc9d7da387b0867aca50330d5b7
Kenf-Azapmo <azapmokenfack@gmail.com>
2026-06-19 02:14:24 +0100

    uploading the main manifest file for the bastion module

---
+++ DevOps-Project-01/infrastructure/modules/bastion/main.tf
@@ -0,0 +1,18 @@
+# Bastion Module 
+
+resource "aws_instance" "bastion" { 
+  ami = var.ami_id
+  instance_type = var.instance_type
+  subnet_id = var.public_subnet_id
+
+  vpc_security_group_ids = [var.bastion_sg_id]
+
+  key_name = var.key_name
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "${var.environment}-bastion"
+    Environment =var.environment
+  }
+}   
